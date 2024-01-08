71ac0217d70b5ed7f4482e160d4f3380b1e46702
Hans Wang <hansmikaa@live.cn>
2024-01-08 17:29:21 -0500

    added initial animation with sounds

--- game.js
+++ game.js
@@ -5,7 +5,11 @@ const gamePattern = [];
 const nextSequence = function () {
   const randomNumber = Math.floor(Math.random() * 4)
   const buttonColours = ["red", "blue", "green", "yellow"];
-  randomChosenColour = buttonColours[randomNumber];
+  let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-};
 
+  // animation with audio played
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  const audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+};
