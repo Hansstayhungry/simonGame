3f50898f1efefb631d228dd874a1fc7b139bc669
Hans Wang <hansmikaa@live.cn>
2024-01-10 21:41:18 -0500

    added audio played functions

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 // record game sequence pattern container
 const gamePattern = [];
 
+// storage the user clicks
+const userClickedPattern = [];
+
 // randomly output one of four colours
 const nextSequence = function () {
   const randomNumber = Math.floor(Math.random() * 4)
@@ -12,4 +15,22 @@ const nextSequence = function () {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   const audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+};
+
+// handler of user chosen colour
+const userChosenColour = function () {
+  $("div[type='button']").on("click", function () {
+    let clickedButton = $(this).attr("id");
+    userClickedPattern.push(clickedButton);
+  });
+};
+
+// play sound effect 
+const playSound = function () {
+  // animation with audio played
+  $("div[type='button']").on("click", function () {
+    $("#" + $(this).attr("id")).fadeIn(100).fadeOut(100).fadeIn(100);
+    const audio = new Audio("sounds/" + $(this).attr("id") + ".mp3");
+    audio.play();
+  });
 };
